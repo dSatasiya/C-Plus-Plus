e29a4426fc796a8d18793de7df98173c09c4f16c
Dhruv Satasiya <100504799+dSatasiya@users.noreply.github.com>
2022-09-07 11:30:55 +0530

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,12 @@
+// This is the basic code of C++ programming..
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello world"<<endl;
+    cout<<"My name is Dhruv"<<endl;
+    cout<<"This is the basic code of C++ Programming.."<<endl;
+    return 0;
+}
