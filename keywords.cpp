20550da03bf81a594837ed72a4baa77614681605
Dhruv Satasiya <100504799+dSatasiya@users.noreply.github.com>
2022-09-10 10:00:59 +0530

    Create keywords.cpp

---
+++ keywords.cpp
@@ -0,0 +1,19 @@
+// keywords in C++..
+// The concept of keywords in C++ is almost same as C.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"...HELLO WORLD..."<<endl;
+    unsigned int a =42949673; 
+    signed int b = 2147483648;
+    char c = 'D';
+    float d = 5.345567654;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    return 0;
+}
