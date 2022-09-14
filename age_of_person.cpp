ed86ee89e438bcca9316110294793d1339693a65
Dhruv Satasiya <100504799+dSatasiya@users.noreply.github.com>
2022-09-14 12:43:55 +0530

    Create age_of_person.cpp

---
+++ age_of_person.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char name[30];
+    int age;
+    cout<<"Enter the name of the person : ";
+    cin>>name;
+    cout<<"Enter the age of the person : ";
+    cin>>age;
+    
+    cout<< "The age of "<<name<<" is "<<age<<endl;
+    
+    return 0;
+}
