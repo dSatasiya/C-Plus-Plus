a3461f88c338a5ab10b539edadaafb038aa22a33
Dhruv Satasiya <100504799+dSatasiya@users.noreply.github.com>
2022-10-02 10:26:12 +0530

    Create addition_of_numbers.cpp

---
+++ addition_of_numbers.cpp
@@ -0,0 +1,15 @@
+// WAP to add two numbers in C++.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, add=0;
+    cout<<"Enter the value of a and b : ";
+    cin>>a>>b;
+    add = a+b;
+    cout<<"The addition of two numbers is : "<<add<<endl;
+    
+    return 0;
+}
