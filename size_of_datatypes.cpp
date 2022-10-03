33230d75c5767013861baa4690492a2c1d86ce67
Dhruv Satasiya <100504799+dSatasiya@users.noreply.github.com>
2022-10-03 11:12:31 +0530

    Create size_of_datatypes.cpp

---
+++ size_of_datatypes.cpp
@@ -0,0 +1,17 @@
+// WAP in c++ to find the sizes of different data types.
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a;
+    float b;
+    char c;
+    double d;
+    cout<<"Size of integer is : "<<sizeof(a)<<endl;
+    cout<<"Size of float is : "<<sizeof(b)<<endl;
+    cout<<"Size of character is : "<<sizeof(c)<<endl;
+    cout<<"Size of double is : "<<sizeof(d)<<endl;
+    return 0;
+}
