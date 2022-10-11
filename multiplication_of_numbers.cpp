21977ae87f748223a7685745aa4fd96b3564a2ae
Dhruv Satasiya <100504799+dSatasiya@users.noreply.github.com>
2022-10-11 12:00:36 +0530

    Create multiplication_of_numbers.cpp

---
+++ multiplication_of_numbers.cpp
@@ -0,0 +1,14 @@
+// C++ Program to Multiply two Numbers
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, mul=1;
+    cout<<"Enter two numbers to multiply : ";
+    cin>>a>>b;
+    mul = a*b;
+    cout<<"Multiplication of numbers is : "<<mul<<endl;
+    return 0;
+}
