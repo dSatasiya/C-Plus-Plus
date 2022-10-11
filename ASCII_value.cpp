0ddeb8e692cb28e1d1362f27f40aa94e16875ed9
Dhruv Satasiya <100504799+dSatasiya@users.noreply.github.com>
2022-10-11 11:57:15 +0530

    Create ASCII_value.cpp

---
+++ ASCII_value.cpp
@@ -0,0 +1,17 @@
+// C++ Program to Find ASCII Value of a Character.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char a;
+    cout<<"Enter a character : ";
+    cin>>a;
+    cout<<"ASCII value of "<<a<<" is "<<int(a);
+    // cout<<"Character is : "<<a<<endl;
+    // cout<<int(a);
+    // int a=10;
+    // cout<<"Value is : "<<a<<endl;
+    return 0;
+}
